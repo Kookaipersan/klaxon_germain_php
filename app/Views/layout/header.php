57434b96a8ba8b14e15468e5984aa91488762821
Kookaipersan <kookaipersan@gmail.com>
2025-08-15 11:47:44 +0200

    ajout du style bootstrap avec sass

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -11,8 +11,10 @@ $user     = $isLogged ? $_SESSION['user'] : null;
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>Touche pas au Klaxon</title>
-  <!-- Bootstrap CSS + Icons -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+ 
+ <link rel="stylesheet" href="<?= $base ?>/css/custom.css">
+
+
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 </head>
 <body class="bg-light">
